b028566907f5a4bd9fa8d66ea5f8e1745150d753
phaskins <p.haskins95@gmail.com>
2016-09-27 20:13:46 -0400

    Update views.py

--- project2/gradingsite/grading/views.py
+++ project2/gradingsite/grading/views.py
@@ -44,8 +44,8 @@ def instructorassignment(request, instructor_id, course_id, assignment_id):
 	today = timezone.now()
 	course = Instructor.objects.get(pk=instructor_id).course_set.filter(pk=course_id)
 	
-	active_sa_list = Assignment.objects.get(pk=assignment_id).studentassignment_set.filter(assignment__due_date >= today)
-	past_sa_list = Assignment.objects.get(pk=assignment_id).studentassignment_set.filter(assignment__due_date < today)
+	active_sa_list = Assignment.objects.get(pk=assignment_id).studentassignment_set.filter(due_date >= today)
+	past_sa_list = Assignment.objects.get(pk=assignment_id).studentassignment_set.filter(due_date < today)
 	context = { 'active_sa_list': active_sa_list, 'past_sa_list': past_sa_list, 'course': course }
         return render(request, 'grading/instructorassignment.html', context)
 
