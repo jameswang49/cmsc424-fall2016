95ac84e834c754ed5bb074cbfa0a0fd506ab3058
phaskins <p.haskins95@gmail.com>
2016-09-30 23:10:46 -0400

    Update views.py

--- project2/gradingsite/grading/views.py
+++ project2/gradingsite/grading/views.py
@@ -76,16 +76,17 @@ def instructorgradesubmission(request, instructor_id, course_id, assignment_id, 
 	if request.method == 'POST':
 		sa_list = Student.objects.get(pk=student_id).studentassignment_set.filter(assignment__id = assignment_id)
 		
-		new_score = int(request.POST['score_given'])
-		sa_list[1].score = new_score
-		sa_list[1].save()
+		score = request.POST['score_given']
+		new_score = int(score)
+		sa_list[0].score = new_score
+		sa_list[0].save()
 		return HttpResponseRedirect(reverse('instructorassignment', args=(instructor_id, course_id, assignment_id)))
 		
 	else:
 		student_obj = Student.objects.get(pk=student_id)
 		sa_list = Assignment.objects.get(pk=assignment_id).studentassignment_set.filter(student__id = student_id)
 	
-		answer_list = sa_list[1].answers.split()
+		answer_list = sa_list[0].answers.split()
 		question_list = Assignment.objects.get(pk=assignment_id).question_set.all()
 	
 		qa_list = zip(question_list, answer_list)
