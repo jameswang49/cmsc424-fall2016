fee48231c1018e015e2ef97bab48562c782f32d5
phaskins <p.haskins95@gmail.com>
2016-10-13 23:27:04 -0400

    Update trigger.sql

--- project3/trigger.sql
+++ project3/trigger.sql
@@ -41,15 +41,7 @@ CREATE OR REPLACE FUNCTION updateFlightCount() RETURNS trigger AS $updateFlight$
 			old_flight_count integer;
 			customer_name char(30);
 			
-		BEGIN
-			SELECT numflights into old_flight_count
-			FROM NumberOfFlightsTaken
-			WHERE customerid = NEW.customerid;
-			
-			SELECT name into customer_name
-			FROM customers
-			WHERE customerid = NEW.customerid;
-		
+		BEGIN				
 			IF (TG_OP = 'INSERT') THEN
 				IF EXISTS (SELECT customerid from NumberOfFlightsTaken
 				    WHERE customerid = NEW.customerid) THEN
@@ -57,18 +49,29 @@ CREATE OR REPLACE FUNCTION updateFlightCount() RETURNS trigger AS $updateFlight$
 					SET numflights = numflights + 1
 					WHERE customerid = NEW.customerid;
 				ELSE	
+					SELECT name into customer_name
+					FROM customers
+					WHERE customerid = NEW.customerid;
+				
 					INSERT INTO NumberOfFlightsTaken
 					(customerid, customername, numflights)
 					values(NEW.customerid,customer_name,1);
 				END IF;
 		
-			ELSEIF (TG_OP = 'DELETE' AND old_flight_count = 1) THEN
-				DELETE FROM NumberOfFlightsTaken
-				WHERE customerid = NEW.customerid;
-			ELSE 
-				UPDATE NumberOfFlightsTaken
-				SET numflights = numflights - 1
-				WHERE customerid = NEW.customerid;
+			ELSEIF (TG_OP = 'DELETE') THEN
+			
+				SELECT numflights into old_flight_count
+				FROM NumberOfFlightsTaken
+				WHERE customerid = OLD.customerid;
+				
+				IF (old_flight_count = 1) THEN 
+					DELETE FROM NumberOfFlightsTaken
+					WHERE customerid = NEW.customerid;
+				ELSE 
+					UPDATE NumberOfFlightsTaken
+					SET numflights = numflights - 1
+					WHERE customerid = NEW.customerid;
+				END IF;
 			END IF;
 			
 		RETURN NEW;
