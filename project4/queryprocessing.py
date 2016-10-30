598eb3b37943a3450b26420393910aeaa4336ad1
phaskins <p.haskins95@gmail.com>
2016-10-30 01:07:28 -0400

    Update queryprocessing.py

--- project4/queryprocessing.py
+++ project4/queryprocessing.py
@@ -173,7 +173,7 @@ class GroupByAggregate(Operator):
 				return min(current_aggregate, new_value)
 			
 		elif aggregate_function == GroupByAggregate.AVERAGE:
-			if new_value is None:
+			if not new_value is None:
 				print "Is None for some reason"
 				
 			return current_aggregate.append(new_value)
