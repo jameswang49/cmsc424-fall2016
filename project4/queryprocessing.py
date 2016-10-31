3ad6019b3f3714fe89a1052cffcbf13c1f350512
phaskins <p.haskins95@gmail.com>
2016-10-30 20:07:36 -0400

    Update queryprocessing.py

--- project4/queryprocessing.py
+++ project4/queryprocessing.py
@@ -383,12 +383,12 @@ class SortMergeJoin(Operator):
 
 				while ptr_l2 < len(left_input):
 					if left_input[ptr_l2].getAttribute(self.left_attribute) > r_attr and found == 0:
-						for r in set_R:
-							output = list(r.t)
-							for i in range (0, left_schema_len):
-								output.insert(0, None)
-							yield Tuple(None, output)
-							break
+						output = list(right_input[ptr_r2].t)
+						for i in range (0, left_schema_len):
+							output.insert(0, None)
+						yield Tuple(None, output)
+						break
+						
 					elif left_input[ptr_l2].getAttribute(self.left_attribute) == r_attr:
 						found = 1
 						
