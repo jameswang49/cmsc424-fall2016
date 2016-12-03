5cebce912cbe302b7fc19d750bb2722baef2c498
phaskins <p.haskins95@gmail.com>
2016-12-03 18:38:40 -0500

    Update transactions.py

--- project6/transactions.py
+++ project6/transactions.py
@@ -140,7 +140,7 @@ class LockTable:
 						i = i + 1
 				i = 0
 				
-			LockTable.find_cycles(waits_for_graph.keys()[0], waits_for_graph, [], [], transactions_to_abort)
+			LockTable.find_cycles(waits_for_graph.keys[0], waits_for_graph, [], [], transactions_to_abort)
 		
 		return transactions_to_abort
 
